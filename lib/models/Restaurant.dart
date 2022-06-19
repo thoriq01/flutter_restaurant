30a0d8647e11fbca7475018fc2fc5b71f7e4d0c7
thoriq01 <alifathoni518@gmail.com>
2022-06-19 16:18:51 +0700

    first commit

---
+++ lib/models/Restaurant.dart
@@ -0,0 +1,61 @@
+class Restaurant {
+  List<Restaurants>? restaurants;
+
+  Restaurant({this.restaurants});
+
+  Restaurant.fromJson(Map<String, dynamic> json) {
+    if (json['restaurants'] != null) {
+      restaurants = <Restaurants>[];
+      json['restaurants'].forEach((v) {
+        restaurants!.add(new Restaurants.fromJson(v));
+      });
+    }
+  }
+}
+
+class Restaurants {
+  String? id;
+  String? name;
+  String? description;
+  String? pictureId;
+  String? city;
+  double? rating;
+  Menus? menus;
+
+  Restaurants({this.id, this.name, this.description, this.pictureId, this.city, this.rating, this.menus});
+
+  Restaurants.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    pictureId = json['pictureId'];
+    city = json['city'];
+    rating = json['rating'];
+    menus = json['menus'] != null ? new Menus.fromJson(json['menus']) : null;
+  }
+}
+
+class Menus {
+  List<Foods>? foods;
+
+  Menus({this.foods});
+
+  Menus.fromJson(Map<String, dynamic> json) {
+    if (json['foods'] != null) {
+      foods = <Foods>[];
+      json['foods'].forEach((v) {
+        foods!.add(new Foods.fromJson(v));
+      });
+    }
+  }
+}
+
+class Foods {
+  String? name;
+
+  Foods({this.name});
+
+  Foods.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+}
